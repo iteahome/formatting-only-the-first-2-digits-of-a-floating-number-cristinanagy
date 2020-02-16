29ad91dd9458ccc482f9e40cca27fa1a8d51ab2a
cristinanagy <58230925+cristinanagy@users.noreply.github.com>
2020-02-16 02:45:18 +0200

    First Commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,11 @@
+public class Application {
+
+    public static void main(String[] args) {
+
+        double pi = 7.122333;
+
+        String str = String.format("%.2f", pi);
+        System.out.println( str);
+
+    }
+}
